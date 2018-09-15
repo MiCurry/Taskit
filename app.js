e8aa66ec1a3d1a26d6b35d81b6d10995be535f10
LHansson <lhanssonatx@gmail.com>
2018-09-15 12:14:09 -0500

    added db info

--- app.js
+++ app.js
@@ -2,15 +2,22 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var session = require('express-session');
-var app = express();
+var app = express();require('dotenv').config()
 
-require('dotenv').config()
 
-app.set('view engine', 'html');
-app.set('views', __dirname + '/views')
 
-var routes = require('./routes/index');
-app.user('/', routes);
+
+let dev_db_url = 'mongodb://TaskItTeam:User11>@ds257752.mlab.com:57752/tasks';
+let mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
 
 const mongoose = require('mongoose');
 
